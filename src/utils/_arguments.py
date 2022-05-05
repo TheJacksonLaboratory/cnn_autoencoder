3e343e02a704e861a069c8da97446f32c669fdd6
Fernando Cervantes Sanchez <iie.fercer@gmail.com>
2022-05-05 11:20:58 -0400

    Updated argument names in _arguments.py and included model selection as well

--- src/utils/_arguments.py
+++ src/utils/_arguments.py
@@ -4,7 +4,7 @@ import torch
 import json
 import argparse
 
-from ._info import DATASETS, SEG_MODELS, CAE_MODELS, CAE_CRITERIONS, SEG_CRITERIONS, SCHEDULERS
+from ._info import DATASETS, SEG_MODELS, CAE_MODELS, PROJ_MODELS, CAE_CRITERIONS, SEG_CRITERIONS, SCHEDULERS
 
 
 def override_config_file(parser):
@@ -244,7 +244,8 @@ def get_project_args(parser_only=False):
     parser.add_argument('-rs', '--seed', type=int, dest='seed', help='Seed for random number generators', default=-1)
     
     parser.add_argument('-ld', '--logdir', type=str, dest='log_dir', help='Directory where all logging and model checkpoints are stored', default='.')
-    parser.add_argument('-m', '--model', type=str, dest='model', help='The checkpoint of the model to be tested')
+    parser.add_argument('-m', '--model', type=str, dest='trained_model', help='The checkpoint of the model to be tested')
+    parser.add_argument('-mt', '--model-type', type=str, dest='model_type', help='Type of autoencoder model', choices=PROJ_MODELS, defualt=PROJ_MODELS[0])
 
     parser.add_argument('-bs', '--batch', type=int, dest='batch_size', help='Patched segmentation in batches if this size', default=1)
     
