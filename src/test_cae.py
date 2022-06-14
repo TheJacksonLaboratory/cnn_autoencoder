99a562a7a609cca9c9bcdf375fa892651c57ccfb
Fernando Cervantes Sanchez <iie.fercer@gmail.com>
2022-06-14 13:46:26 -0400

    Added some times measurements for profiling the test module
    The deltaE_cie76 has been used instead of the deltaE_cie2000 because it is less time intensive.

--- src/test_cae.py
+++ src/test_cae.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from time import perf_counter
-from skimage.color import deltaE_ciede2000, rgb2lab
+from skimage.color import deltaE_cie76, rgb2lab
 
 import numpy as np
 import zarr
@@ -17,22 +17,38 @@ from numcodecs import Blosc
 model_options = {"AutoEncoder": models.AutoEncoder, "MaskedAutoEncoder": models.MaskedAutoEncoder}
 
 def compute_deltaCIELAB(x=None, x_r=None, y_q=None):
-    return np.mean(deltaE_ciede2000(rgb2lab(np.moveaxis(x, 1, -1)), rgb2lab(np.moveaxis(x_r, 1, -1))))
+    convert_x_time = perf_counter()
+    x_lab = rgb2lab(np.moveaxis(x, 1, -1))
+    convert_x_time = perf_counter() - convert_x_time
+    
+    convert_r_time = perf_counter()
+    x_r_lab = rgb2lab(np.moveaxis(x_r, 1, -1))
+    convert_r_time = perf_counter() - convert_r_time
+    
+    delta_cielab_time = perf_counter()
+    delta_cielab = deltaE_cie76(x_lab, x_r_lab)
+    delta_cielab_time = perf_counter() - delta_cielab_time
+    
+    mean_delta_cielab_time = perf_counter()
+    mean_delta_cielab = np.mean(delta_cielab)
+    mean_delta_cielab_time = perf_counter() - mean_delta_cielab_time
+    
+    return mean_delta_cielab, dict(convert_x=convert_x_time, convert_r=convert_r_time, delta_cielab=delta_cielab_time, mean_delta_cielab=mean_delta_cielab_time, delta_shape_ndim=delta_cielab.ndim, delta_shape_b=delta_cielab.shape[0], delta_shape_x=delta_cielab.shape[1], delta_shape_y=delta_cielab.shape[2])
 
 
 def compute_psnr(x=None, x_r=None, y_q=None):
-    rmse = compute_rmse(x=x, x_r=x_r)
-    return 20 * np.log10(1.0 / rmse)
+    rmse, _ = compute_rmse(x=x, x_r=x_r)
+    return 20 * np.log10(1.0 / rmse), None
 
 
 def compute_rmse(x=None, x_r=None, y_q=None):
-    return np.sqrt(np.mean((x_r - x)**2))
+    return np.sqrt(np.mean((x_r - x)**2)), None
 
 
 def compute_rate(x=None, x_r=None, y_q=None):
     # Check compression directly from the information of the zarr file
     z_y_q = zarr.array(data=y_q, chunks=(1, y_q.shape[1], y_q.shape[2], y_q.shape[3]), dtype='u1', compressor=Blosc(cname='zlib', clevel=9, shuffle=Blosc.BITSHUFFLE))
-    return float(z_y_q.nbytes_stored) / np.prod(x.shape)
+    return float(z_y_q.nbytes_stored) / np.prod(x.shape), None
 
 """
 Available metrics (can add more later):
@@ -69,8 +85,18 @@ def test(cae_model, data, args):
     all_metrics = dict([(m_k, []) for m_k in metric_fun])
     all_metrics['time'] = []
 
+    load_times = []
+    eval_times = []
+    metrics_eval_times = dict([(m_k, []) for m_k in metric_fun])
+    
+    all_extra_info = {}
+    
     with torch.no_grad():
+        load_time = perf_counter()
         for i, (x, _) in enumerate(data):
+            load_time = perf_counter() - load_time
+            load_times.append(load_time)
+            
             e_time = perf_counter()
             x_r, y, _ = cae_model(x)
             y_q = y + 127.5
@@ -81,15 +107,29 @@ def test(cae_model, data, args):
             x = 0.5 * x + 0.5
 
             x_r = x_r.detach().cpu().numpy()
-            x = x.detach().cpu().numpy()
+            x = x.detach().cpu().numpy()            
             y_q = y_q.detach().cpu().numpy()
 
+            eval_time = perf_counter()
             for m_k in metric_fun.keys():
-                score = metric_fun[m_k](x=x, x_r=x_r, y_q=y_q)
+                metrics_eval_time = perf_counter()
+                score, extra_info = metric_fun[m_k](x=x, x_r=x_r, y_q=y_q)
+                metrics_eval_time = perf_counter() - metrics_eval_time
+                metrics_eval_times[m_k].append(metrics_eval_time)
+                
+                if extra_info is not None:
+                    for e_k in extra_info.keys():
+                        if all_extra_info.get(e_k, None) is None:
+                            all_extra_info[e_k] = []
+                        
+                        all_extra_info[e_k].append(extra_info[e_k])
+                
                 if score > 0.0:
                     all_metrics[m_k].append(score)
                 else:
                     all_metrics[m_k].append(np.nan)
+            eval_time = perf_counter() - eval_time
+            eval_times.append(eval_time)
             
             all_metrics['time'].append(e_time)
 
@@ -99,7 +139,21 @@ def test(cae_model, data, args):
                     avg_metric = np.nanmean(all_metrics[m_k])
                     avg_metrics += '%s=%0.4f ' % (m_k, avg_metric)
                 logger.debug('\t[{:04d}/{:04d}] Test metrics {}'.format(i, len(data), avg_metrics))
+            
+            load_time = perf_counter()
     
+    logger.debug('Loading avg. time: {:0.4f} (+-{:0.4f}), evaluation avg. time: {:0.4f}(+-{:0.4f})'.format(np.mean(load_times), np.std(load_times), np.mean(eval_times), np.std(eval_times)))
+    
+    for m_k in metrics_eval_times.keys():
+        avg_eval_time = np.mean(metrics_eval_times[m_k])
+        std_eval_time = np.std(metrics_eval_times[m_k])
+        logger.debug('\tEvaluation of {} avg. time: {:0.4f} (+- {:0.4f})'.format(m_k, avg_eval_time, std_eval_time))    
+    
+    for e_k in all_extra_info.keys():
+        avg_ext_time = np.mean(all_extra_info[e_k])
+        std_ext_time = np.std(all_extra_info[e_k])
+        logger.debug('\tExtra info of {} avg. time: {:0.4f} (+- {:0.4f})'.format(e_k, avg_ext_time, std_ext_time))
+        
     all_metrics_stats = {}
     for m_k in all_metrics.keys():
         avg_metric = np.nanmean(all_metrics[m_k])
@@ -110,7 +164,9 @@ def test(cae_model, data, args):
 
         all_metrics_stats[m_k + '_stats'] = dict(avg=avg_metric, std=std_metric, med=med_metric, min=min_metric, max=max_metric)
 
-    return all_metrics.update(all_metrics_stats)
+    all_metrics.update(all_metrics_stats)
+    
+    return all_metrics
 
 
 def setup_network(args):
@@ -176,6 +232,10 @@ def main(args):
     
     all_metrics_stats = test(cae_model, test_data, args)
     torch.save(all_metrics_stats, os.path.join(args.log_dir, 'metrics_stats_%s%s.pth' % (args.seed, args.log_identifier)))
+    
+    for m_k in list(metric_fun.keys()) + ['time']:
+        avg_metrics = '%s=%0.4f (+-%0.4f)' % (m_k, all_metrics_stats[m_k + '_stats']['avg'], all_metrics_stats[m_k + '_stats']['std'])
+        logger.debug('==== Test metrics {}'.format(avg_metrics))
 
 
 if __name__ == '__main__':
