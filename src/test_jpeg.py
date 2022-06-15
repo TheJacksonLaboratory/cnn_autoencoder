077478e11342cd8d09376decea2b91fd8701a1de
Fernando Cervantes Sanchez <iie.fercer@gmail.com>
2022-06-15 10:30:04 -0400

    Added comparison with jpeg2k using the same format for cae models evaluation

--- src/test_jpeg.py
+++ src/test_jpeg.py
@@ -10,20 +10,23 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-import models
+from numcodecs import Blosc, register_codec
+from imagecodecs.numcodecs import Jpeg2k, JpegXl
+import imageio
+
 import utils
-from numcodecs import Blosc
 
-model_options = {"AutoEncoder": models.AutoEncoder, "MaskedAutoEncoder": models.MaskedAutoEncoder}
+register_codec(Jpeg2k)
+register_codec(JpegXl)
 
 
-def compute_deltaCIELAB(x=None, x_r=None, y_q=None):
+def compute_deltaCIELAB(x=None, x_r=None):
     convert_x_time = perf_counter()
-    x_lab = rgb2lab(np.moveaxis(x, 1, -1))
+    x_lab = rgb2lab(x)
     convert_x_time = perf_counter() - convert_x_time
     
     convert_r_time = perf_counter()
-    x_r_lab = rgb2lab(np.moveaxis(x_r, 1, -1))
+    x_r_lab = rgb2lab(x_r[:])
     convert_r_time = perf_counter() - convert_r_time
     
     delta_cielab_time = perf_counter()
@@ -34,22 +37,21 @@ def compute_deltaCIELAB(x=None, x_r=None, y_q=None):
     mean_delta_cielab = np.mean(delta_cielab)
     mean_delta_cielab_time = perf_counter() - mean_delta_cielab_time
     
-    return mean_delta_cielab, dict(convert_x=convert_x_time, convert_r=convert_r_time, delta_cielab=delta_cielab_time, mean_delta_cielab=mean_delta_cielab_time, delta_shape_ndim=delta_cielab.ndim, delta_shape_b=delta_cielab.shape[0], delta_shape_x=delta_cielab.shape[1], delta_shape_y=delta_cielab.shape[2])
+    return mean_delta_cielab, dict(convert_x=convert_x_time, convert_r=convert_r_time, delta_cielab=delta_cielab_time, mean_delta_cielab=mean_delta_cielab_time, delta_shape_ndim=delta_cielab.ndim, delta_shape_x=delta_cielab.shape[0], delta_shape_y=delta_cielab.shape[1])
 
 
-def compute_psnr(x=None, x_r=None, y_q=None):
+def compute_psnr(x=None, x_r=None):
     rmse, _ = compute_rmse(x=x, x_r=x_r)
     return 20 * np.log10(1.0 / rmse), None
 
 
-def compute_rmse(x=None, x_r=None, y_q=None):
-    return np.sqrt(np.mean((x_r/255.0 - x/255.0)**2)), None
+def compute_rmse(x=None, x_r=None):
+    return np.sqrt(np.mean((x_r[:]/255. - x/255.)**2)), None
 
 
-def compute_rate(x=None, x_r=None, y_q=None):
+def compute_rate(x=None, x_r=None):
     # Check compression directly from the information of the zarr file
-    z_y_q = zarr.array(data=y_q, chunks=(1, y_q.shape[1], y_q.shape[2], y_q.shape[3]), dtype='u1', compressor=Blosc(cname='zlib', clevel=9, shuffle=Blosc.BITSHUFFLE))
-    return float(z_y_q.nbytes_stored) / np.prod(x.shape), None
+    return float(x_r.nbytes_stored) / np.prod(x.shape), None
 
 """
 Available metrics (can add more later):
@@ -61,14 +63,12 @@ Available metrics (can add more later):
 metric_fun = {'dist': compute_rmse, 'rate':compute_rate, 'psnr': compute_psnr, 'delta_cielab': compute_deltaCIELAB}
 
 
-def test(cae_model, data, args):
+def test(data, args):
     """ Test step.
-    Evaluates the performance of the network in its current state using the full set of test elements.
+    Evaluates the performance of the JPEG compressor.
 
     Parameters
     ----------
-    cae_model : torch.nn.Module
-        The network model in the current state
     data : torch.utils.data.DataLoader or list[tuple]
         The test dataset. Because the target is recosntruct the input, the label associated is ignored
     args: Namespace
@@ -80,8 +80,7 @@ def test(cae_model, data, args):
         Dictionary with the computed metrics         
     """
     logger = logging.getLogger(args.mode + '_log')
-
-    cae_model.eval()
+    compressor = Jpeg2k(level=5)
 
     all_metrics = dict([(m_k, []) for m_k in metric_fun])
     all_metrics['time'] = []
@@ -102,23 +101,17 @@ def test(cae_model, data, args):
 
             n_examples += x.size(0)
             
+            x = 127.5 * x[0] + 127.5
+            x = x.detach().cpu().to(torch.uint8).numpy().transpose(1, 2, 0)
+
             e_time = perf_counter()
-            x_r, y, _ = cae_model(x)
-            y_q = y + 127.5
-            y_q = y_q.round()
-            x_r = 127.5 * x_r.clip(-1.0, 1.0) + 127.5
+            x_r = zarr.array(data=x, chunks=tuple(x.shape), dtype='u1', compressor=compressor)
             e_time = perf_counter() - e_time
-
-            x = 127.5 * x + 127.5
-
-            x_r = x_r.detach().cpu().to(torch.uint8).numpy()
-            x = x.detach().cpu().to(torch.uint8).numpy()            
-            y_q = y_q.detach().cpu().to(torch.uint8).numpy()
 
             eval_time = perf_counter()
             for m_k in metric_fun.keys():
                 metrics_eval_time = perf_counter()
-                score, extra_info = metric_fun[m_k](x=x, x_r=x_r, y_q=y_q)
+                score, extra_info = metric_fun[m_k](x=x, x_r=x_r)
                 metrics_eval_time = perf_counter() - metrics_eval_time
                 metrics_eval_times[m_k].append(metrics_eval_time)
                 
@@ -176,42 +169,6 @@ def test(cae_model, data, args):
     return all_metrics
 
 
-def setup_network(args):
-    """ Setup a nerual network for image compression/decompression.
-
-    Parameters
-    ----------
-    args : Namespace
-        The input arguments passed at running time. All the parameters are passed directly to the model constructor.
-        This way, the constructor can take the parameters needed that have been passed by the user.
-    
-    Returns
-    -------
-    cae_model : nn.Module
-        The convolutional neural network autoencoder model.
-    """
-
-    # The autoencoder model contains all the modules
-    if not args.gpu:
-        checkpoint_state = torch.load(args.trained_model, map_location=torch.device('cpu'))
-    else:
-        checkpoint_state = torch.load(args.trained_model)
-
-    cae_model = model_options[checkpoint_state['args']['model_type']](**checkpoint_state['args'])
-
-    # If there are more than one GPU, DataParallel handles automatically the distribution of the work
-    cae_model = nn.DataParallel(cae_model)
-    if args.gpu:
-        cae_model.cuda()
-
-    cae_model.module.embedding.load_state_dict(checkpoint_state['embedding'])
-    cae_model.module.analysis.load_state_dict(checkpoint_state['encoder'])
-    cae_model.module.synthesis.load_state_dict(checkpoint_state['decoder'])
-    cae_model.module.fact_entropy.load_state_dict(checkpoint_state['fact_ent'])
-
-    return cae_model
-
-
 def main(args):
     """ Set up the training environment
 
@@ -221,12 +178,6 @@ def main(args):
         The set of parameters passed to the different constructors to set up the convolutional autoencoder training
     """
     logger = logging.getLogger(args.mode + '_log')
-
-    cae_model = setup_network(args)
-
-    # Log the training setup
-    logger.info('Network architecture:')
-    logger.info(cae_model)
     
     # Generate a dataset from a single image to divide in patches and iterate using a dataloader
     transform, _ = utils.get_zarr_transform(normalize=True)
@@ -234,13 +185,15 @@ def main(args):
     if not args.source_format.startswith('.'):
         args.source_format = '.' + args.source_format
 
+    args.batch_size = 1
+
     zarr_ds = utils.ZarrDataset(root=args.data_dir, dataset_size=1000000, mode=args.mode_data, patch_size=args.patch_size, offset=0, transform=transform, source_format=args.source_format, workers=args.workers)
     test_data = DataLoader(zarr_ds, batch_size=args.batch_size, num_workers=args.workers, shuffle=args.shuffle_test, pin_memory=True, worker_init_fn=utils.zarrdataset_worker_init)
     
     if args.test_size < 0:
         args.test_size = len(zarr_ds)
 
-    all_metrics_stats = test(cae_model, test_data, args)
+    all_metrics_stats = test(test_data, args)
     torch.save(all_metrics_stats, os.path.join(args.log_dir, 'metrics_stats_%s%s.pth' % (args.seed, args.log_identifier)))
     
     for m_k in list(metric_fun.keys()) + ['time']:
