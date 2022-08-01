Fixed a bug that caused an incomplete compression due to a miss-calculation of the number of patches an image can be sub-divided

src/compress.py
@@ -1,4 +1,3 @@
-from genericpath import isdir
 import logging
 import os
 
@@ -77,6 +76,7 @@ def compress_image(comp_model, input_filename, output_filename, channels_bn, com
     data_queue = DataLoader(zarr_ds, batch_size=batch_size, num_workers=0, shuffle=False, pin_memory=True, worker_init_fn=utils.zarrdataset_worker_init)
 
     H, W = zarr_ds._imgs_shapes[0]
+    comp_H, comp_W = int(np.ceil(H/2**comp_level)), int(np.ceil(W/2**comp_level))
     channels_org = zarr_ds.get_channels()
     
     comp_patch_size = patch_size//2**comp_level
@@ -109,7 +109,7 @@ def compress_image(comp_model, input_filename, output_filename, channels_bn, com
     
     if stitch_batches:
         z_comp = comp_group.create_dataset(zarr_ds._data_group, 
-                shape=(1, channels_bn, 1, int(np.ceil(H/2**comp_level)), int(np.ceil(W/2**comp_level))), 
+                shape=(1, channels_bn, 1, comp_patch_size * int(np.ceil(comp_H / comp_patch_size)), comp_patch_size * int(np.ceil(comp_W / comp_patch_size))), 
                 chunks=(1, channels_bn, 1, comp_patch_size, comp_patch_size), 
                 dtype='u1', compressor=compressor)
     else:
@@ -134,7 +134,8 @@ def compress_image(comp_model, input_filename, output_filename, channels_bn, com
                     _, tl_y, tl_x = utils.compute_grid(i*batch_size + k, imgs_shapes=[(H, W)], imgs_sizes=[0, len(zarr_ds)], patch_size=patch_size)
                     tl_y *= comp_patch_size
                     tl_x *= comp_patch_size
-                    z_comp[0, :, 0, tl_y:tl_y + comp_patch_size, tl_x:tl_x + comp_patch_size] = y_k
+
+                    z_comp[0, :, 0, tl_y:tl_y+comp_patch_size, tl_x:tl_x + comp_patch_size] = y_k
             else:
                 z_comp[i*batch_size:i*batch_size+x.size(0), :, 0, :, :] = y_q
 

src/utils/datasets/_zarrbased.py
@@ -259,15 +259,10 @@ def get_patch(z, tl_y, tl_x, patch_size, offset=0):
         pad_left = tl_x - tl_x_offset
         pad_right = br_x_offset - br_x
 
-        valid_pad_up = min(pad_up, br_y - tl_y)
-        valid_pad_down = min(pad_down, br_y - tl_y)
-        valid_pad_left = min(pad_left, br_x - tl_x)
-        valid_pad_right = min(pad_right, br_x - tl_x)
-
         patch  = np.pad(patch, 
             (*leading_padding, 
-             (valid_pad_up, valid_pad_down),
-             (valid_pad_left, valid_pad_right)),
+             (pad_up, pad_down),
+             (pad_left, pad_right)),
             mode='symmetric', reflect_type='even')
 
     return patch
@@ -461,7 +456,7 @@ class ZarrDataset(Dataset):
 
     def _compute_size(self, z_list, rois_list):
         imgs_shapes = [((roi[-2].stop - roi[-2].start)//roi[-2].step, (roi[-1].stop - roi[-1].start)//roi[-1].step) for _, roi in rois_list]
-        imgs_sizes = np.cumsum([0] + [int(np.ceil((H * W) / self._patch_size**2)) for H, W in imgs_shapes])
+        imgs_sizes = np.cumsum([0] + [int(np.ceil(H/ self._patch_size)) * int(np.ceil(W / self._patch_size)) for H, W in imgs_shapes])
         
         # Get the upper bound of patches that can be obtained from all zarr files (images with smaller size will be padded)
         max_H, max_W = np.max(np.array(imgs_shapes), axis=0)