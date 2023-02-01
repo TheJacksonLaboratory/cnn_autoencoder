Corrected computation of distortion in the test module, from RMSE to MSE, and added schemes to the configuration of lLC compressed models

src/compress.py
@@ -1,5 +1,6 @@
 from dask.diagnostics import ProgressBar
 
+import time
 import logging
 import os
 import shutil
@@ -52,7 +53,8 @@ def compress_image(comp_model, input_filename, output_filename, channels_bn,
                    data_group='0/0',
                    data_axes='TCZYX',
                    seed=None,
-                   comp_label='compressed'):
+                   comp_label='compressed',
+                   progress_bar=False):
 
     compressor = Blosc(cname='zlib', clevel=9, shuffle=Blosc.BITSHUFFLE)
     fn, rois = utils.parse_roi(input_filename, source_format)
@@ -162,9 +164,16 @@ def compress_image(comp_model, input_filename, output_filename, channels_bn,
     else:
         component = data_group
 
-    with ProgressBar():
-        y.to_zarr(output_filename, component=component, overwrite=True,
-                  compressor=compressor)
+    e_time = time.perf_counter()
+    if progress_bar:
+        with ProgressBar():
+            y.to_zarr(output_filename, component=component, overwrite=True,
+                    compressor=compressor)
+    else:
+            y.to_zarr(output_filename, component=component, overwrite=True,
+                    compressor=compressor)
+    e_time = time.perf_counter() - e_time
+    print('Compression time:', e_time)
 
     # Add metadata to the compressed zarr file
     group = zarr.open(output_filename)
@@ -244,28 +253,28 @@ def setup_network(state, use_gpu=False, lc_pretrained_model=None,
 
     cae_model_base.embedding.load_state_dict(state['embedding'])
     cae_model_base.analysis.load_state_dict(state['encoder'])
-    cae_model_base.fact_entropy.load_state_dict(state['fact_ent'])
+    # cae_model_base.fact_entropy.load_state_dict(state['fact_ent'])
 
     if lc_pretrained_model is not None and ft_pretrained_model is not None:
 
         # Load the model checkpoint from its compressed version
         lc_compressed_model_state = torch.load(lc_pretrained_model,
                                                map_location='cpu')
-        ft_compressed_model_state = torch.load(ft_pretrained_model,
-                                               map_location='cpu')['model_state']
+        ft_compressed_model_checkpoint = torch.load(ft_pretrained_model,
+                                                    map_location='cpu')
 
         cae_model_base = nn.DataParallel(cae_model_base)
         cae_model_base = utils.load_compressed_dict(cae_model_base,
                                                     lc_compressed_model_state,
-                                                    ft_compressed_model_state,
-                                                    conv_scheme='scheme_2')
+                                                    ft_compressed_model_checkpoint['model_state'],
+                                                    conv_scheme=ft_compressed_model_checkpoint['config']['conv_scheme'])
         cae_model_base = cae_model_base.module
 
     embedding = cae_model_base.embedding
     analysis = cae_model_base.analysis
-    fact_ent = cae_model_base.fact_entropy
+    # fact_ent = cae_model_base.fact_entropy
 
-    comp_model = nn.Sequential(embedding, analysis, fact_ent)
+    comp_model = nn.Sequential(embedding, analysis) #, fact_ent)
 
     comp_model = nn.DataParallel(comp_model)
     if use_gpu:
@@ -327,7 +336,8 @@ def compress(args):
             data_axes=args.data_axes,
             data_group=args.data_group,
             seed=state['args']['seed'],
-            comp_label=args.task_label_identifier)
+            comp_label=args.task_label_identifier,
+            progress_bar=args.progress_bar)
 
         logger.info('Compressed image %s into %s' % (in_fn, out_fn))
 

src/decompress.py
@@ -1,5 +1,6 @@
 from dask.diagnostics import ProgressBar
 
+import time
 import logging
 import os
 import shutil
@@ -92,7 +93,8 @@ def decompress_image(decomp_model, input_filename, output_filename,
                      min_range=0.0,
                      max_range=1.0,
                      range_offset=0.0,
-                     range_scale=1.0):
+                     range_scale=1.0,
+                     progress_bar=False):
     compressor = Blosc(cname='zlib', clevel=9, shuffle=Blosc.BITSHUFFLE)
     fn, rois = utils.parse_roi(input_filename, '.zarr')
     src_group = zarr.open(fn, mode='r')
@@ -256,9 +258,17 @@ def decompress_image(decomp_model, input_filename, output_filename,
         comp_pyr = '/'.join(component.split('/')[:-1])
         for r, y_r in enumerate(y_pyr):
             comp_r = comp_pyr + '/%i' % r
-            with ProgressBar():
-                y_r.to_zarr(output_filename, component=comp_r, overwrite=True,
-                            compressor=compressor)
+            e_time = time.perf_counter()
+            if progress_bar:
+                with ProgressBar():
+                    y_r.to_zarr(output_filename, component=comp_r, overwrite=True,
+                                compressor=compressor)
+            else:
+                    y_r.to_zarr(output_filename, component=comp_r, overwrite=True,
+                                compressor=compressor)
+
+            e_time = time.perf_counter() - e_time
+            print('Reconstruction time:', e_time)
 
         group = zarr.open(output_filename)
         if len(decomp_label):
@@ -354,21 +364,21 @@ def setup_network(state, rec_level=-1, compute_pyramids=False, use_gpu=False,
     cae_model_base.synthesis.load_state_dict(state['decoder'], strict=False)
 
     if state['args']['version'] == '0.5.5':
-        for color_layer in cae_model_base.color_layers:
+        for color_layer in cae_model_base.synthesis.color_layers:
             color_layer[0].weight.data.copy_(state['decoder']['synthesis_track.4.weight'])
 
     if lc_pretrained_model is not None and ft_pretrained_model is not None:
         # Load the model checkpoint from its compressed version
         lc_compressed_model_state = torch.load(lc_pretrained_model,
                                                map_location='cpu')
-        ft_compressed_model_state = torch.load(ft_pretrained_model,
-                                               map_location='cpu')['model_state']
+        ft_compressed_model_checkpoint = torch.load(ft_pretrained_model,
+                                               map_location='cpu')
 
         cae_model_base = nn.DataParallel(cae_model_base)
         cae_model_base = utils.load_compressed_dict(cae_model_base,
                                                     lc_compressed_model_state,
-                                                    ft_compressed_model_state,
-                                                    conv_scheme='scheme_2')
+                                                    ft_compressed_model_checkpoint['model_state'],
+                                                    conv_scheme=ft_compressed_model_checkpoint['config']['conv_scheme'])
         cae_model_base = cae_model_base.module
 
     decomp_model = cae_model_base.synthesis
@@ -390,7 +400,7 @@ def decompress(args):
     # Open checkpoint from trained model state
     state = utils.load_state(args)
 
-    if state['args']['normalize']:
+    if state['args'].get('normalize', True):
         min_range = -1.0
         max_range = 1.0
         range_scale = 0.5

src/models/_criterions.py
@@ -175,7 +175,6 @@ class DistortionLoss(DistortionLossBase):
 class MSSSIMLoss(DistortionLossBase):
     def __init__(self, patch_size, scale=1, **kwargs):
         super().__init__(**kwargs)
-
         if ((11 - 2 * scale) - patch_size // 2 ** (scale + 4)) > 0:
             self.padding = nn.ZeroPad2d(
                 ((11 - 2 * scale) - patch_size // 2 ** (scale + 4)) * 2 ** 3)
@@ -193,10 +192,12 @@ class MSSSIMLoss(DistortionLossBase):
             normalize='relu')
 
     def compute_dist(self, x, x_r, **kwargs):
+        padded_x_r = self.padding(x_r)
+        padded_x = self.padding(x.to(x_r.device))
         ms_ssim = self.msssim.to(x_r.device)(
-            self._range_scale * self.padding(x_r)
+            self._range_scale * padded_x_r
             + self._range_offset,
-            self._range_scale * self.padding(x.to(x_r.device))
+            self._range_scale * padded_x
             + self._range_offset)
 
         ms_ssim = 1.0 - ms_ssim

src/models/tasks/_autoencoders.py
@@ -433,7 +433,7 @@ class Analyzer(nn.Module):
                                     bias=bias,
                                     padding_mode='reflect'))
 
-        down_track.append(nn.Hardtanh(min_val=-127.5, max_val=127.5,
+        down_track.append(nn.Hardtanh(min_val=-127, max_val=127,
                                       inplace=False))
 
         self.analysis_track = nn.Sequential(*down_track)
@@ -531,13 +531,13 @@ class Synthesizer(nn.Module):
             return x_brg
 
         fx = self.color_layers[-1](fx)
-        fx = fx.clip_(self._min_range, 1)
+        # fx.clip_(self._min_range, 1)
         return fx, x_brg
 
     def forward(self, x):
         x = self.synthesis_track(x)
         x = self.color_layers[-1](x)
-        x = x.clip_(self._min_range, 1)
+        # x.clip_(self._min_range, 1)
         return x
 
 
@@ -561,7 +561,7 @@ class SynthesizerInflate(Synthesizer):
                                          self.color_layers):
             fx = up_layer(fx)
             x_r = color_layer(fx)
-            x_r = x_r.clip_(self._min_range, 1)
+            #x_r.clip_(self._min_range, 1)
             x_r_ms.insert(0, x_r)
 
         return x_r_ms
@@ -638,14 +638,22 @@ class AutoEncoder(nn.Module):
 
         elif factorized_entropy_only:
             # When running on factorized entropy only mode, use x as y_q
-            # log_p_y = self.fact_entropy(x)
-            log_p_y = self.fact_entropy(x + 0.5) - self.fact_entropy(x - 0.5) + 1e-12
+            log_p_y = self.fact_entropy(x)
             return log_p_y
 
         fx = self.embedding(x)
 
         y_q, y = self.analysis(fx)
-        p_y = self.fact_entropy(y_q + 0.5) - self.fact_entropy(y_q - 0.5) + 1e-12
+        lower = self.fact_entropy(y_q - 0.5)
+        upper = self.fact_entropy(y_q + 0.5)
+
+        sign = -torch.sign(lower + upper)
+        sign = sign.detach()
+
+        p_y = torch.abs(
+            torch.sigmoid(sign * upper) - torch.sigmoid(sign * lower)
+        )
+        p_y = F.hardtanh(p_y, min_val=1e-9)
 
         x_r = self.synthesis(y_q)
 

src/test_cae.py
@@ -51,13 +51,13 @@ def compute_ms_ssim(x=None, x_r=None, **kwargs):
         reduction='elementwise_mean',
         k1=0.01,
         k2=0.03,
-        data_range=1,
+        data_range=255,
         betas=(0.0448, 0.2856, 0.3001, 0.2363, 0.1333),
         normalize='relu'
     )
     ms_ssim = ms_ssim_fn(
-        torch.from_numpy(np.moveaxis(x_r, -1, 0)[np.newaxis]).float() / 255.0,
-        torch.from_numpy(np.moveaxis(x, -1, 0)[np.newaxis]).float() / 255.0)
+        torch.from_numpy(np.moveaxis(x_r, -1, 0)[np.newaxis]).float(),
+        torch.from_numpy(np.moveaxis(x, -1, 0)[np.newaxis]).float())
     return ms_ssim, None
 
 
@@ -71,9 +71,9 @@ def compute_psnr(x=None, x_r=None, **kwargs):
     return psnr, None
 
 
-def compute_rmse(x=None, x_r=None, **kwargs):
-    rmse = np.sqrt(mean_squared_error(x / 255.0, x_r / 255.0))
-    return rmse, None
+def compute_mse(x=None, x_r=None, **kwargs):
+    mse = mean_squared_error(x, x_r)
+    return mse, None
 
 
 def compute_rate(x=None, x_r=None, y_q_ptr=None, **kwargs):
@@ -82,14 +82,14 @@ def compute_rate(x=None, x_r=None, y_q_ptr=None, **kwargs):
 
 
 """Available metrics (can add more later):
-    dist=Distortion (RMSE)
+    dist=Distortion (MSE)
     rate=Compression rate (bits-per-pixel bpp)
     ssim=Structural similarity
     psnr=Peak Dignal-to-Noise Ratio (dB)
-    delta_cielab=Distance between images in the CIELAB color space (RMSE in
+    delta_cielab=Distance between images in the CIELAB color space (MSE in
     CIELAB space)
 """
-metric_fun = {'dist': compute_rmse,
+metric_fun = {'dist': compute_mse,
               'rate': compute_rate,
               'ms-ssim': compute_ms_ssim,
               'ssim': compute_ssim,
@@ -205,7 +205,7 @@ def test_cae(args):
 
     state = utils.load_state(args)
 
-    if state['args']['normalize']:
+    if state['args'].get('normalize', True):
         min_range = -1.0
         max_range = 1.0
         range_scale = 0.5
@@ -216,10 +216,14 @@ def test_cae(args):
         range_scale = 1.0
         range_offset = 0.0
 
-    comp_model = compress.setup_network(state, args.use_gpu)
+    comp_model = compress.setup_network(state, args.use_gpu,
+                                        lc_pretrained_model=args.lc_pretrained_model,
+                                        ft_pretrained_model=args.ft_pretrained_model)
     decomp_model = decompress.setup_network(state, rec_level=-1,
                                             compute_pyramids=False,
-                                            use_gpu=args.use_gpu)
+                                            use_gpu=args.use_gpu,
+                                            lc_pretrained_model=args.lc_pretrained_model,
+                                            ft_pretrained_model=args.ft_pretrained_model)
 
     # Log the training setup
     logger.info('Network architecture:')

src/train_cae_ms.py
@@ -67,7 +67,7 @@ def valid(cae_model, data, criterion, args):
             if args.print_log:
                 q.set_description(
                     'Validation Loss {:.4f} (dist=[{}], rate={:.2f}, '
-                    'aux={:.2f}, energy={:.3f}). Quant bn [{:.2f}, {:.2f}] '
+                    'aux={:.2f}, energy={:.3f}). Quant bn [{:.2f}, {:.2f}] ({:.2f},{:.2f})'
                     '({:.2f}, {:.2f}, {:.2f}), '
                     'rec [{:.2f}, {:.2f}]'.format(
                         sum_loss / (i+1),
@@ -77,6 +77,8 @@ def valid(cae_model, data, criterion, args):
                         loss_dict.get('energy', 0.0),
                         y.detach().min(),
                         y.detach().max(),
+                        p_y.detach().min(),
+                        p_y.detach().max(),
                         *[quant.item() for quant in cae_model.module.fact_entropy.quantiles.detach().mean(dim=(0, 1))],
                         x_r[0].detach().min(),
                         x_r[0].detach().max()))
@@ -85,7 +87,7 @@ def valid(cae_model, data, criterion, args):
                 logger.debug(
                     '\t[{:04d}/{:04d}] Validation Loss {:.4f} (dist=[{}], '
                     'rate={:.2f}, aux={:.2f}, energy={:.3f}). '
-                    'Quant bn [{:.2f}, {:.2f}] '
+                    'Quant bn [{:.2f}, {:.2f}] ({:.2f},{:.2f}) '
                     '({:.2f}, {:.2f}, {:.2f}), '
                     'rec [{:.2f}, {:.2f}]'.format(
                         i,
@@ -97,6 +99,8 @@ def valid(cae_model, data, criterion, args):
                         loss_dict.get('energy', 0.0),
                         y.detach().min(),
                         y.detach().max(),
+                        p_y.detach().min(),
+                        p_y.detach().max(),
                         *[quant.item() for quant in cae_model.module.fact_entropy.quantiles.detach().mean(dim=(0, 1))],
                         x_r[0].detach().min(),
                         x_r[0].detach().max()))
@@ -204,7 +208,7 @@ def train(cae_model, train_data, valid_data, criterion, stopping_criteria,
                         q_penalty.set_description(
                             'Sub-iter Loss {:.4f} (dist=[{}], rate={:.2f}, '
                             'aux={:.2f}, energy={:.3f}) '
-                            'Quant bn [{:.2f}, {:.2f}] '
+                            'Quant bn [{:.2f}, {:.2f}] ({:.2f},{:.2f}) '
                             '({:.2f}, {:.2f}, {:.2f}), '
                             'rec [{:.2f}, {:.2f}]'.format(
                                 step_loss,
@@ -214,6 +218,8 @@ def train(cae_model, train_data, valid_data, criterion, stopping_criteria,
                                 loss_dict.get('energy', 0.0),
                                 y.detach().min(),
                                 y.detach().max(),
+                                p_y.detach().min(),
+                                p_y.detach().max(),
                                 *[quant.item() for quant in cae_model.module.fact_entropy.quantiles.detach().mean(dim=(0, 1))],
                                 x_r[0].detach().min(),
                                 x_r[0].detach().max()))
@@ -241,7 +247,7 @@ def train(cae_model, train_data, valid_data, criterion, stopping_criteria,
             if args.print_log:
                 q.set_description(
                     'Training Loss {:.4f} (dist=[{}], rate={:.2f}, aux={:.2f},'
-                    ' energy={:3f}). Quant bn [{:.2f}, {:.2f}] '
+                    ' energy={:3f}). Quant bn [{:.2f}, {:.2f}] ({:.2f},{:.2f})'
                     '({:.2f}, {:.2f}, {:.2f}), '
                     'rec [{:.2f}, {:.2f}]'.format(
                         sum_loss / (i+1),
@@ -251,6 +257,8 @@ def train(cae_model, train_data, valid_data, criterion, stopping_criteria,
                         loss_dict.get('energy', 0.0),
                         y.detach().min(),
                         y.detach().max(),
+                        p_y.detach().min(),
+                        p_y.detach().max(),
                         *[quant.item() for quant in cae_model.module.fact_entropy.quantiles.detach().mean(dim=(0, 1))],
                         x_r[0].detach().min(),
                         x_r[0].detach().max()))
@@ -262,7 +270,7 @@ def train(cae_model, train_data, valid_data, criterion, stopping_criteria,
                     logger.debug(
                         '\n\t[Step {:06d} {:04d}/{:04d}] Training Loss {:.4f} '
                         '(dist=[{}], rate={:.2f}, aux={:.2f}'
-                        'energy={:3f} ). Quant bn [{:.2f}, {:.2f}] '
+                        'energy={:3f} ). Quant bn [{:.2f}, {:.2f}] ({:.2f},{:.2f}) '
                         '({:.2f}, {:.2f}, {:.2f}), '
                         'rec [{:.2f}, {:.2f}]'.format(
                             step, i, len(train_data),
@@ -273,6 +281,8 @@ def train(cae_model, train_data, valid_data, criterion, stopping_criteria,
                             loss_dict.get('energy', 0.0),
                             y.detach().min(),
                             y.detach().max(),
+                            p_y.detach().min(),
+                            p_y.detach().max(),
                             *[quant.item() for quant in cae_model.module.fact_entropy.quantiles.detach().mean(dim=(0, 1))],
                             x_r[0].detach().min(),
                             x_r[0].detach().max()))

src/utils/lcutils/_utils.py
@@ -169,7 +169,7 @@ def load_compressed_dict(model_base, lc_checkpoint, ft_checkpoint,
 
     for i, module in enumerate(
             [x for x in model_base.modules() if isinstance(x, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear))]):
-        module.selected_rank_ = compression_info[f"task_{i}"]['selected_rank']
+        module.selected_rank_ = compression_info[f"low-rank-{i}"]['selected_rank']
         logger.debug(module.selected_rank_)
 
     old_weight_decay = True